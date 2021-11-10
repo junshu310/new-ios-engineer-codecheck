58f77d5f614990a899a441a247f1184db0beb1d6
junshu310 <shunki1107@gmail.com>
2021-11-10 17:08:43 +0900

    Fix-VC-code

--- iOSEngineerCodeCheck/ViewController.swift
+++ iOSEngineerCodeCheck/ViewController.swift
@@ -9,84 +9,81 @@
 import UIKit
 
 class ViewController: UITableViewController, UISearchBarDelegate {
-
-    @IBOutlet weak var SchBr: UISearchBar!
     
-    var repo: [[String: Any]]=[]
+    @IBOutlet weak var repositorySearchBar: UISearchBar!
+    
+    var responses: [[String : Any]] = []
     
     var task: URLSessionTask?
-    var word: String!
-    var url: String!
-    var idx: Int!
+    var keyword: String = ""
+    var index: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        SchBr.text = "GitHubのリポジトリを検索できるよー"
-        SchBr.delegate = self
-    }
-    
-    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
-        // ↓こうすれば初期のテキストを消せる
-        searchBar.text = ""
-        return true
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        task?.cancel()
+        repositorySearchBar.placeholder = "GitHubのリポジトリを検索できるよー"
+        repositorySearchBar.delegate = self
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-        word = searchBar.text!
+        keyword = searchBar.text!.removeWhitespacesAndNewlines
         
-        if word.count != 0 {
-            url = "https://api.github.com/search/repositories?q=\(word!)"
-            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
-                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
-                    if let items = obj["items"] as? [[String: Any]] {
-                    self.repo = items
-                        DispatchQueue.main.async {
-                            self.tableView.reloadData()
-                        }
+        if keyword.count != 0 {
+            let url = "https://api.github.com/search/repositories?q=\(keyword)"
+            let encordURL = url.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+            task = URLSession.shared.dataTask(with: URL(string: encordURL!)!) { (data, res, err) in
+                
+                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any],
+                   let items = obj["items"] as? [[String: Any]] {
+                    self.responses = items
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
                     }
                 }
             }
-        // これ呼ばなきゃリストが更新されません
-        task?.resume()
+            // これ呼ばなきゃリストが更新されません
+            task?.resume()
         }
-        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "Detail"{
-            let dtl = segue.destination as! ViewController2
-            dtl.vc1 = self
+            let detail = segue.destination as? ViewController2
+            detail?.vc1 = self
         }
-        
     }
     
+    
+    //MARK: TABLEVIEW CELL
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repo.count
+        return responses.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell()
-        let rp = repo[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "Repository")
+        let response = responses[indexPath.row]
+        cell.textLabel?.text = response["full_name"] as? String ?? ""
+        print(response["language"] as? String ?? "")
+        cell.detailTextLabel?.text = response["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
-        
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
-        idx = indexPath.row
+        index = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
-        
     }
-    
+}
+
+
+extension StringProtocol where Self: RangeReplaceableCollection {
+    ///スペースを取り除き、新しい文を返す
+    var removeWhitespacesAndNewlines: Self {
+        filter { !$0.isNewline && !$0.isWhitespace }
+    }
 }
